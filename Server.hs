78694c1a8a97a541d8dc846db71f1d4613902884
Christophe Thibaut <cthibaut@octo.com>
2012-08-29 12:54:22 +0200

    read and increments an access counter since server stated
    first working example of a server updating its internal state

--- Server.hs
+++ Server.hs
@@ -2,20 +2,23 @@
 {-# LANGUAGE FlexibleContexts #-}
 module Main where
 import Control.Monad.Trans
+import Control.Concurrent.STM
 import Happstack.Server (nullConf, simpleHTTP, toResponse, ok)
 import Happstack.Server.Internal.Types
 import Happstack.Server.Internal.Monads
 import Happstack.Server.Response
 
-readCounter :: IO Integer
-readCounter = do s <- readFile "counter.txt"
-                 let c = (read s)
-                 return c
 
-response :: ServerPartT IO Response
-response = do c <- lift $ readCounter  -- execute readCounter in the wrapped IO monad
-              ok $ toResponse $ "Access Counter: " ++ (show c)
+readAndUpdate :: TVar Integer -> IO Integer
+readAndUpdate var = atomically $ do n <- readTVar var
+                                    writeTVar var (n+1)
+                                    return n
+
+
+response :: TVar Integer -> ServerPartT IO Response
+response counter = do c <- lift $ readAndUpdate counter
+                      ok $ toResponse $ "Access count since server started: " ++ (show c)
 
 main :: IO ()
-main = do
-  simpleHTTP nullConf $ response
+main = do counter <- atomically $ newTVar 0
+          simpleHTTP nullConf $ response counter
